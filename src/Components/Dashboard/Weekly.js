57a6f161eb62f6691d7f23f31bbe3d46e294b145
pullip lee <pullip.lee1023@gmail.com>
2020-07-16 22:33:40 +0800

    pulling master

---
+++ src/Components/Dashboard/Weekly.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const Weekly = () => {
+    return (
+        <div>
+           <p className="center bold gray70">This week's best post</p>
+            <div className="row">
+                <div className="col s12 m6">
+                    <div className="card darken-1">
+                        <div className="card-content grey-text">
+                            <h4 className="card-title">Blog post 1</h4>
+                            <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div className="card-action center">
+                            <a href="#" className="bold green-text">View more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Weekly
