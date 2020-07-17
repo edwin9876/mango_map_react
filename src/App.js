1e6078e79c1b5543823115e3644f3c42bc87819c
Ch-oi <gmu1233@gmail.com>
2020-07-18 03:26:15 +0800

    Resolved conflicts with mapstyle from Pullip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import Home from './Components/Screen/HomeScreen';
@@ -7,24 +6,10 @@ import Map from './Components/Screen/MapScreen';
 import MapDetails from './Components/Map/MapDetails';
 import Blog from './Components/Screen/BlogScreen';
 import Chat from './Components/Screen/ChatScreen';
+import ChatDetails from './Components/Chat/ChatDetails';
 import Profile from './Components/Screen/ProfileScreen';
 import SignIn from './Components/Auth/SignIn';
 import SignUp from './Components/Auth/SignUp';
-=======
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Navbar from './Components/Layout/Navbar'
-import Home from './Components/Screen/HomeScreen'
-import Map from './Components/Screen/MapScreen'
-import MapDetails from './Components/Map/MapDetails'
-import Blog from './Components/Screen/BlogScreen'
-import Chat from './Components/Screen/ChatScreen'
-import ChatDetails from './Components/Chat/ChatDetails'
-import Profile from './Components/Screen/ProfileScreen'
-import SignIn from './Components/Auth/SignIn'
-import SignUp from './Components/Auth/SignUp'
-
-
->>>>>>> ec83c521d9eae51b69bb4d641c284aa20f77c204
 
 class App extends Component {
   render() {
@@ -37,12 +22,8 @@ class App extends Component {
             <Route exact path='/map' component={Map} />
             <Route exact path='/map/:id' component={MapDetails} />
             <Route exact path='/blog' component={Blog} />
-<<<<<<< HEAD
             <Route exact path='/chat' component={Chat} />
-=======
-            <Route exact path='/chat' component={Chat}/>
-            <Route exact path='/chat/:id' component={ChatDetails}/>
->>>>>>> ec83c521d9eae51b69bb4d641c284aa20f77c204
+            <Route exact path='/chat/:id' component={ChatDetails} />
             <Route exact path='/profile' component={Profile} />
             <Route exact path='/signin' component={SignIn} />
             <Route exact path='/signup' component={SignUp} />
