00021e6533b703c0f4ff512c3e04ee43aa4f3d04
Ch-oi <gmu1233@gmail.com>
2020-07-19 05:47:22 +0800

    Public folder missing

--- tests/MapService.test.js
+++ tests/MapService.test.js
@@ -8,7 +8,7 @@ import { MapContainer } from '../src/Containers/Map/Map';
 configure({ adapter: new Adapter() });
 
 describe('A test case for test case...', () => {
-  it("Wouldn't it be nice if I am wealthy, then I don't have to wait so long", () => {
+  it('Testing test suites', () => {
     let wrapper = shallow(<MapContainer />);
     console.log(wrapper);
     expect(wrapper.find(Marker).toHaveLength(11));
