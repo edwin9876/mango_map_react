cd088a6634bb54e7bea12c9e174e3656b00f921b
YIN <anotherone@YINde-Air.lan>
2020-07-21 18:41:30 +0800

    action creator & reducer async call (create&fetch)

--- src/redux/actions/chatroom.js
+++ src/redux/actions/chatroom.js
@@ -12,39 +12,57 @@ import {
     FETCH_CHATROOM,
 } from '../constants/action-types'
 
-export function createChatroom(payload){
-    return { type:CREATE_CHATROOM,payload}
+import axios from 'axios'
+
+export function fetchAllChatroom() {
+    return dispatch => {
+        let res = await axios('http://localhost:8000/chatroom/all')
+        dispatch({ type: FETCH_ALLCHATROOM, payload: res.data })
+    }
 }
 
-export function createChatroomRecord(payload){
-    return { type:CREATE_CHATROOMRECORD,payload}
+export function fetchChatroom(payload) {
+    return dispatch => {
+        let res = await axios(`http://localhost:8000/chatroom/${payload.chatroom_id}`)
+        dispatch({ type: FETCH_CHATROOM, payload: res.data })
+    }
+}
+export function createChatroom(payload) {
+    return dispatch => {
+
+        let res = await axios.post(`http://localhost:8000/chatroom/`, payload)
+        dispatch({ type: CREATE_CHATROOM, payload: res.data })
+    }
+}
+export function createChatroomRecord(payload) {
+    return dispatch => {
+
+        let res = await axios.post(`http://localhost:8000/chatroom/${payload.chatroom_id}/record`, payload.chatRecord)
+        dispatch({ type: CREATE_CHATROOMRECORD, payload: res.data })
+    }
 }
 
-export function createChatroomUser(payload){
-    return { type:CREATE_CHATROOMUSER,payload}
+
+export function updateChatroom(payload) {
+    return { type: UPDATE_CHATROOM, payload }
 }
 
-
-export function updateChatroom(payload){
-    return { type:UPDATE_CHATROOM,payload}
+export function updateChatroomRecord(payload) {
+    return { type: UPDATE_CHATROOMRECORD, payload }
 }
 
-export function updateChatroomRecord(payload){
-    return { type:UPDATE_CHATROOMRECORD,payload}
+export function updateChatroomUser(payload) {
+    return { type: UPDATE_CHATROOMUSER, payload }
 }
 
-export function updateChatroomUser(payload){
-    return { type:UPDATE_CHATROOMUSER,payload}
+export function removeChatroom(payload) {
+    return { type: REMOVE_CHATROOM, payload }
 }
 
-export function removeChatroom(payload){
-    return { type:REMOVE_CHATROOM,payload}
+export function removeChatroomRecord(payload) {
+    return { type: REMOVE_CHATROOMRECORD, payload }
 }
 
-export function removeChatroomRecord(payload){
-    return { type:REMOVE_CHATROOMRECORD,payload}
-}
-
-export function removeChatroomUser(payload){
-    return { type:REMOVE_CHATROOMUSER,payload}
+export function removeChatroomUser(payload) {
+    return { type: REMOVE_CHATROOMUSER, payload }
 }
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,14 +1,57 @@
 import {
-    CREATE_NEWCATEGORY,
-    CREATE_BLOG,
-    CREATE_COMMENT,
-    REMOVE_COMMENT,
-    UPDATE_BLOG,
-    UPDATE_BLOGCATEGORY,
-    UPDATE_BLOGIMAGE,
-    REMOVE_BLOG,
-    REMOVE_BLOGCATEGORY,
-    REMOVE_BLOGIMAGE,
-    FETCH_ALLBLOG,
-    FETCH_BLOG,
+    CREATE_USER,
+    FETCH_ALLUSER,
+    FETCH_USER,
+    UPDATE_USER,
+    REMOVE_USER,
+    CREATE_FAVBLOG,
+    REMOVE_FAVBLOG,
+    CREATE_USERDISTRICT,
+    REMOVE_USERDISTRICT,
+    CREATE_USERCHAT,
+    REMOVE_USERCHAT,
+    CREATE_USERCHATRECORD
 } from '../constants/action-types'
+
+const initialUserState = {
+    users:[],
+    user:[]
+}
+
+const userReducer = (state=initialUserState,action) =>{
+    switch (action.type){
+        case FETCH_ALLUSER:
+            return {
+                ...state,
+                users:[...action.payload]
+            }
+        case FETCH_USER:
+            return {
+                ...state,
+                user:[...action.payload]
+            }
+        case CREATE_USER:
+            return {
+                ...state
+            }
+        case CREATE_FAVBLOG:
+            return {
+                ...state
+            }
+        case CREATE_USERDISTRICT:
+            return {
+                ...state
+            }
+        case CREATE_USERCHAT:
+            return {
+                ...state
+            }
+        case CREATE_USERCHATRECORD:
+            return {
+                ...state
+            }
+
+    }
+}
+
+export default userReducer
