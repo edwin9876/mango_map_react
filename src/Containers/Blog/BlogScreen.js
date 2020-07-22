5d8fec04a1cbed4d783f1387667d66b7ced50e54
pullip lee <pullip.lee1023@gmail.com>
2020-07-22 16:15:52 +0800

    blog part change

--- src/Containers/Blog/BlogScreen.js
+++ src/Containers/Blog/BlogScreen.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react'
 import SearchBar from '../../Components/UI/Layout/SearchBar'
-import Posts from './Posts/Posts'
 import TopPics from '../../Components/UI/Dashboard/TopPics'
 import TopUsers from '../../Components/UI/Dashboard/TopUsers'
 import WeeklyPic from '../../Components/UI/Dashboard/WeeklyPic'
 import WeeklyPost from '../../Components/UI/Dashboard/WeeklyPost'
+import BlogList from './BlogList/BlogList'
+import { connect } from 'react-redux'
 
 
 
 
-export default class BlogScreen extends Component {
+class BlogScreen extends Component {
     render() {
+
+        const { blogs } = this.props;
+
         return (
             <div className="mb10vh">
                 <SearchBar />
-                <WeeklyPost/>
+                <WeeklyPost />
                 <WeeklyPic />
 
                 <div className="card-tabs">
                     <ul className="tabs tabs-fixed-width">
-                    <li className="tab"><a href="#test4" className="bold black-text">New posts</a></li>
+                        <li className="tab"><a href="#test4" className="bold black-text">New posts</a></li>
                         <li className="tab"><a href="#test6" className="bold black-text">Pictures</a></li>
                         <li className="tab"><a href="#test4" className="bold black-text">Top users</a></li>
                     </ul>
                 </div>
+                <BlogList blogs={blogs}/>
                 {/* <TopPics /> */}
-                <TopUsers/>
+                {/* <TopUsers/> */}
             </div>
         )
     }
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+        blogs: state.blog.blogs
+    }
+}
+
+export default connect(mapStateToProps)(BlogScreen)
