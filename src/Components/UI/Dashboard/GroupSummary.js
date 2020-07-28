59bfb4537826d421828edb46602e7c4032dd3bf0
YIN <anotherone@YINde-Air.lan>
2020-07-28 21:31:14 +0800

    connect create post to backend

--- src/Components/UI/Dashboard/GroupSummary.js
+++ src/Components/UI/Dashboard/GroupSummary.js
@@ -4,34 +4,35 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const GroupSummary = (props) => {
   console.log(props)
-    return (
+  return (
 
-      <ThemeContext.Consumer>{(context) => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
+    <ThemeContext.Consumer>{(context) => {
+      const { isLightTheme, light, dark } = context;
+      const theme = isLightTheme ? light : dark;
 
-        return(
+      return (
         <div className="margin5">
-        <ListGroup>
-        <ListGroupItem color={theme.listcolor} tag="a" href="/chat/:id" className="justify-content-between d-flex">
-        <img className="material-icons roundimg"
-        src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIMey7cyC1XcqtyFcJlNhz7yP4oT1kAahWPw&usqp=CAU'
-        alt='Avatar'/>
-        <h4 className="d-flex align-items-center">{props.chatroom.name}</h4>
-        <h6 className="d-flex align-items-center blur">{props.chatroom.created_at}
-        
-        </h6>
-    
-        </ListGroupItem>
+          <ListGroup>
+            <ListGroupItem color={theme.listcolor} tag="a" href="/chat/:id" className="justify-content-between d-flex">
+              <img className="material-icons roundimg"
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIMey7cyC1XcqtyFcJlNhz7yP4oT1kAahWPw&usqp=CAU'
+                alt='Avatar' />
+              <h4 className="d-flex align-items-center">{props.chatroom.name}</h4>
+              <h6 className="d-flex align-items-center blur">{props.chatroom.created_at}
 
-       
-        </ListGroup>
+              </h6>
 
-           
+            </ListGroupItem>
 
-        </div>) }}
-        </ThemeContext.Consumer>
-    )
+
+          </ListGroup>
+
+
+
+        </div>)
+    }}
+    </ThemeContext.Consumer>
+  )
 }
 
 export default GroupSummary
