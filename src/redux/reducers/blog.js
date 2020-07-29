118c4dd241b6aac05892e53943bef5e4c2e4dc93
YIN <anotherone@YINde-Air.lan>
2020-07-29 17:26:28 +0800

    .

--- src/redux/reducers/blog.js
+++ src/redux/reducers/blog.js
@@ -42,55 +42,9 @@ const initialBlogState = {
     },
   ],
 
-  post: [
-    {
-      id: '4',
-      title: 'Mini Trip in HK',
-      author: 'Pullip123',
-      content: `Hope. Fear. Excitement. Traveling for the first time produced a wave of emotions.
+  post: [],
 
-        When I left to travel the world on my first round-the-world trip, I didn’t know what to expect.
-        
-        Now, with fifteen years of travel experience under my belt, I know better. Traveling is second nature to me now. I land in an airport and I just go on autopilot.
-        
-        But, back then, I was as green as they come.
-        
-        To compensate for my lack of experience, I followed my guidebooks and wet my feet by going on organized tours. I was young and inexperienced and I made a lot of rookie travel mistakes.
-        
-        I know what it’s like to just be starting out and have a mind filled with questions, anxieties, and concerns.
-        
-        So, if you’re new to travel and looking for advice to help you prepare, here are 12 tips that I’d tell a new traveler to help them avoid some of my early mistakes:
-      `,
-      main_picture_url: `https://www.bhg.com.au/media/20109/mangos.jpg`,
-      comments: [
-        { id: '1', user_id: 'Jacky123', body: 'Quite inspiring!' },
-        { id: '2', user_id: 'Edwin123', body: 'Quite surprising!' },
-      ],
-      modified: false,
-      userDistrict_id: '',
-    },
-  ],
-
-  posts: [
-    {
-      id: '1',
-      title: 'My First Trip',
-      author: 'Pullip',
-      content: 'Very nice first trip I had in Hong Kong',
-    },
-    {
-      id: '2',
-      title: 'My Second Trip',
-      author: 'Jacky',
-      content: 'Very nice second trip I had in Hong Kong',
-    },
-    {
-      id: '3',
-      title: 'My Third Trip',
-      author: 'Edwin',
-      content: 'Very nice third trip I had in Hong Kong',
-    },
-  ],
+  posts: [],
 
   categories: [],
 };
