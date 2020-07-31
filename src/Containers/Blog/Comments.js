1a5a04fff2afb146b8033f272e37df019bb834e3
YIN <anotherone@YINde-Air.lan>
2020-07-31 20:38:52 +0800

    edit comment

--- src/Containers/Blog/Comments.js
+++ src/Containers/Blog/Comments.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import LocationStorage from '../Map/PredefinedLocations/LocationStorage';
 
 // unpacking individual comments from comments array
-const IndiComment = ( { comment }) => {
+const IndiComment = ({ comment }) => {
+
+    let user_id = JSON.parse(localStorage.getItem('user')).id
 
     return (
         <div className="d-flex" >
             <p className="bold margin1x">{comment.user_name}</p>
             <p >{comment.body} </p>
-            <i className="margin1x material-icons tiny gray70">edit</i>
-            <i className="material-icons tiny gray70">delete_forever</i>
+            {user_id === comment.user_id &&
+                <div>
+                    <i className="margin1x material-icons tiny gray70">edit</i>
+                    <i className="material-icons tiny gray70">delete_forever</i>
+                </div>
+            }
         </div>
     );
 };
@@ -20,7 +27,7 @@ const Comments = ({ comment }) => {
         <div className="blog-list section">
             {/* in case there's no posts */}
 
-            <IndiComment comment={comment}  />
+            <IndiComment comment={comment} />
 
 
         </div>
