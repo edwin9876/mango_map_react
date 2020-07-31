d69414f1a37aaf0d55ae2751a0f668d63642a1f5
YIN <anotherone@YINde-Air.lan>
2020-07-31 16:17:58 +0800

    blogdetails

--- src/Components/UI/Dashboard/PostSummary.js
+++ src/Components/UI/Dashboard/PostSummary.js
@@ -1,51 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { ThemeContext } from '../../../Contexts/Theme'
 import {
     Card, CardImg, CardBody, CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
 
-const PostSummary = (props) => {
-    console.log(props)
+class PostSummary extends Component {
+    static contextType = ThemeContext;
 
-    return (
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
 
-        <ThemeContext.Consumer>{(context) => {
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark;
-            console.log( props)
+    }
 
-            return (
-                <div>
-                   
-                         <Card className="margin5" style={{ background: theme.low, borderColor: theme.high }}>
+    handleClick = async (e) => {
+        console.log(this.props)
+        this.props.history.push(`/blog/${this.props.posts.id}`);
+        
+
+    }
+
+    render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+
+                return (
+                    <div>
+
+                        <Card className="margin5" style={{ background: theme.low, borderColor: theme.high }}>
                             <CardImg top width="100%" src='' alt="post_img" />
                             <CardBody>
-                                <CardTitle className="bold">{props.posts.title.toUpperCase()}</CardTitle>
-                                <CardSubtitle >{props.posts.created_at.slice(0, 10)}</CardSubtitle>
+                                <CardTitle className="bold">{this.props.posts.title.toUpperCase()}</CardTitle>
+                                <CardSubtitle >{this.props.posts.created_at.slice(0, 10)}</CardSubtitle>
                                 <br />
-                                <a href="/blog/:id" className="d-flex justify-content-center"><Button className="noBorder" style={{ background: theme.highlight }}>View more</Button></a>
+                                <Button onClick={this.handleClick} className="noBorder d-flex justify-content-center" style={{ background: theme.highlight }}>View more</Button>
                             </CardBody>
                         </Card>
-                    
-                </div>)
-        }}
-        </ThemeContext.Consumer>
-    )
-}
 
-// const PostSummary = ( props) => {
-//     return (
-//         <ThemeContext.Consumer>{(context) => {
-//             const { isLightTheme, light, dark } = context;
-//             const theme = isLightTheme ? light : dark;
-//             return (
-//                 <div>
-//                     <MyPost posts={props.posts} />
-//                 </div>)
-//         }}
-//         </ThemeContext.Consumer>
-//     )
-// }
+                    </div>)
+            }
 
-export default PostSummary
+    }
+
+    // const PostSummary = ( props) => {
+    //     return (
+    //         <ThemeContext.Consumer>{(context) => {
+    //             const { isLightTheme, light, dark } = context;
+    //             const theme = isLightTheme ? light : dark;
+    //             return (
+    //                 <div>
+    //                     <MyPost posts={props.posts} />
+    //                 </div>)
+    //         }}
+    //         </ThemeContext.Consumer>
+    //     )
+    // }
+
+    export default PostSummary
