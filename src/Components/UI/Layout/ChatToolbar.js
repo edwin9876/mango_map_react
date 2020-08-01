ae7770458cc7f84e4d0fedd3f024d1ef874348b3
Pullip Lee <pullip.lee1023@gmail.com>
2020-08-01 17:57:47 +0800

    add user in chatroom

--- src/Components/UI/Layout/ChatToolbar.js
+++ src/Components/UI/Layout/ChatToolbar.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form} from 'reactstrap';
 
-const ChatToolbar = ({ backToChatList }) => {
+
+const ChatToolbar = ({ backToChatList }, props) => {
+
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+  const [unmountOnClose, setUnmountOnClose] = useState(true);
+
+  const toggle = () => setModal(!modal);
+  const changeUnmountOnClose = e => {
+      let value = e.target.value;
+      setUnmountOnClose(JSON.parse(value));
+  }
+
   return (
     <div>
       <nav>
-        <div className='d-flex justify-content-between paddingy1'>
-          <i className='material-icons' onClick={backToChatList}>
+        <div className='d-flex justify-content-between margin5'>
+          <i className='material-icons gray50' onClick={backToChatList}>
             arrow_back
           </i>
 
-          <a href='#'>
-            <i className=' material-icons'>add</i>
-          </a>
+          <div>
+          <Form inline onSubmit={(e) => e.preventDefault()}>
+          {' '}
+          <i className='material-icons gray70' onClick={toggle}>person_add{buttonLabel}</i>
+          </Form>
+          <Modal isOpen={modal} toggle={toggle} className={className} unmountOnClose={unmountOnClose}>
+              <ModalHeader toggle={toggle}>Add friends to chat</ModalHeader>
+              <ModalBody>
+                  <Input type="textarea" placeholder="Find with username" rows={1} />
+           
+              <Label for="exampleSelectMulti">Search Result comes here</Label>
+              <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                <option>pullip1</option>
+                <option>pullip12</option>
+              </Input>
+            </ModalBody>
+              <ModalFooter  className="d-flex justify-content-center" >
+                  <Button color="success" onClick={toggle}>Add User</Button>{' '}
+              </ModalFooter>
+          </Modal>
+          </div>
+
         </div>
       </nav>
     </div>
