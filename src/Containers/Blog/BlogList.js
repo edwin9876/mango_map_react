8c01dc8fd313380835ad3dc877da15a5648d1295
YIN <anotherone@YINde-Air.lan>
2020-08-02 13:26:05 +0800

    123

--- src/Containers/Blog/BlogList.js
+++ src/Containers/Blog/BlogList.js
@@ -7,35 +7,36 @@ class ConnectedBlogList extends Component {
   static contextType = ThemeContext;
 
   constructor(props) {
-      super(props)
-      this.state = {
-      }
+    super(props)
+    this.state = {
+    }
   }
 
   handleClick = async (e) => {
     this.props.history.push(`/blog/${this.props.posts.id}`);
-}
-     
-render() {
-  const { isLightTheme, light, dark } = this.context;
-  const theme = isLightTheme ? light : dark;
-  console.log(this.props)
-return (
-        <div className='margin1vw'>
-          {/* in case there's no posts */}
-          <ListGroupItem  onClick={this.handleClick} style={{ cursor: 'pointer', background: theme.low, color: theme.high, borderColor: theme.high }}>
+  }
+
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    console.log(this.props)
+    return (
+      <div className='margin1vw'>
+        {/* in case there's no posts */}
+        <ListGroupItem onClick={this.handleClick} style={{ cursor: 'pointer', background: theme.low, color: theme.high, borderColor: theme.high }}>
           <ListGroupItemHeading className="d-flex justify-content-center">{this.props.posts.title.toUpperCase()}</ListGroupItemHeading>
           <ListGroupItemText className="gray70 d-flex justify-content-center ">{this.props.posts.locationName.toUpperCase()}</ListGroupItemText>
           <ListGroupItemText className="blur d-flex justify-content-center">by {this.props.posts.userName}</ListGroupItemText>
           <p className="blur d-flex justify-content-center">{this.props.posts.created_at.slice(0, 10)}</p>
         </ListGroupItem>
-        </div>)
+      </div>)
 
-}}
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
-      ...state
+    ...state
   }
 }
 
@@ -43,4 +44,4 @@ const mapStateToProps = (state) => {
 const BlogList = connect(mapStateToProps)(ConnectedBlogList)
 
 
-    export default BlogList
+export default BlogList
