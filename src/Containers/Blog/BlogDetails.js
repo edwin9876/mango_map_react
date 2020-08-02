877487e218c7309e79eebebd4e6be822e679f2b9
YIN <anotherone@YINde-Air.lan>
2020-08-02 20:12:17 +0800

    123

--- src/Containers/Blog/BlogDetails.js
+++ src/Containers/Blog/BlogDetails.js
@@ -13,7 +13,6 @@ import { fetchPost, createComment,createFavPost,removeFavPost} from '../../redux
 
 class ConnectedBlogDetails extends Component {
     static contextType = ThemeContext;
-// wec
 
     constructor(props) {
         super(props)
