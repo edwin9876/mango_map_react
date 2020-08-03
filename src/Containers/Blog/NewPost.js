d5915f79bfee3f4c5f2339fc780060b904941794
YIN <anotherone@YINde-Air.lan>
2020-08-04 05:15:22 +0800

    create post link with map

--- src/Containers/Blog/NewPost.js
+++ src/Containers/Blog/NewPost.js
@@ -198,12 +198,14 @@ class ConnectedNewPost extends Component {
         maxWidth: "100px"
       })
     }
+    const label = this.props.history.location.pathname.split('/')[2]
+    const value = parseInt(this.props.history.location.pathname.split('/')[3])
 
     return (
       <div>
         <div className="margin5">
           <Label for="title" className="bold">Choose location</Label>
-          <Select onChange={this.handleChange} name='location_id' styles={customStyles} options={this.state.locations} />
+          <Select defaultValue={{ label: label, value: value }} onChange={this.handleChange} name='location_id' styles={customStyles} options={this.state.locations} />
         </div>
 
         <Form id="createPost" onSubmit={this.handleSubmit} className="uploader margin5" encType="multipart/form-data">
