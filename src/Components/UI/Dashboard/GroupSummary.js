33875dab3e54d104a4bb696f1efda131bf73b0ff
Ch-oi <gmu1233@gmail.com>
2020-08-04 21:26:36 +0800

    ASDJIOA

--- src/Components/UI/Dashboard/GroupSummary.js
+++ src/Components/UI/Dashboard/GroupSummary.js
@@ -1,40 +1,57 @@
-import React from 'react'
-import { ThemeContext } from '../../../Contexts/Theme'
+import React from 'react';
+import { ThemeContext } from '../../../Contexts/Theme';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import { fetchChatroom } from '../../../redux/actions/chatroom';
+import { connect } from 'react-redux';
 
 const GroupSummary = (props) => {
-  console.log(props)
+  console.log(props);
   return (
+    <ThemeContext.Consumer>
+      {(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        const { dispatch } = props;
 
-    <ThemeContext.Consumer>{(context) => {
-      const { isLightTheme, light, dark } = context;
-      const theme = isLightTheme ? light : dark;
+        const handleClick = (e) => {
+          console.log(props.chatroom.id);
+          dispatch(fetchChatroom(props.chatroom.id));
+          console.log(props);
+          return props.history.push(`/chat`);
+          // return props.history.push(`/chat/${props.chatroom.id}`);
+        };
 
-      const handleClick =(e)=>{
-        return props.history.push(`/chat/${props.chatroom.id}`)
-      }
+        return (
+          <div className='margin5'>
+            <ListGroup>
+              <ListGroupItem
+                onClick={handleClick}
+                color={theme.listcolor}
+                tag='a'
+                className='justify-content-between d-flex'
+              >
+                <img
+                  className='material-icons roundimg'
+                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIMey7cyC1XcqtyFcJlNhz7yP4oT1kAahWPw&usqp=CAU'
+                  alt='Avatar'
+                />
+                <h4 className='d-flex align-items-center'>
+                  {props.chatroom.room_name}
+                </h4>
+                <h6 className='d-flex align-items-center blur'>
+                  {props.chatroom.created_at.slice(0, 10)}
+                </h6>
+              </ListGroupItem>
+            </ListGroup>
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
 
-      return (
-        <div className="margin5">
-        <ListGroup >
-        <ListGroupItem onClick={handleClick} color={theme.listcolor} tag="a"  className="justify-content-between d-flex">
-        <img className="material-icons roundimg"
-        src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIMey7cyC1XcqtyFcJlNhz7yP4oT1kAahWPw&usqp=CAU'
-        alt='Avatar'/>
-        <h4 className="d-flex align-items-center">{props.chatroom.room_name}</h4>
-        <h6 className="d-flex align-items-center blur">{props.chatroom.created_at.slice(0, 10)}
-        </h6>
-    
-        </ListGroupItem>
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
 
-       
-        </ListGroup>
-
-           
-
-        </div>) }}
-        </ThemeContext.Consumer>
-    )
-}
-
-export default GroupSummary
+export default connect(null, mapDispatchToProps)(GroupSummary);
--- src/Containers/Profile/ProfileScreen.js
+++ src/Containers/Profile/ProfileScreen.js
@@ -1,163 +1,172 @@
 import React, { Component } from 'react';
-import { ThemeContext } from '../../Contexts/Theme'
-import { connect } from 'react-redux'
+import { ThemeContext } from '../../Contexts/Theme';
+import { connect } from 'react-redux';
 
-import ProfileDetails from './ProfileDetails'
-import TripSummary from '../../Components/UI/Dashboard/TripSummary'
-import GroupSummary from '../../Components/UI/Dashboard/GroupSummary'
-import PostSummary from '../../Components/UI/Dashboard/PostSummary'
-import FavPostSummary from '../../Components/UI/Dashboard/FavPostSummary'
+import ProfileDetails from './ProfileDetails';
+import TripSummary from '../../Components/UI/Dashboard/TripSummary';
+import GroupSummary from '../../Components/UI/Dashboard/GroupSummary';
+import PostSummary from '../../Components/UI/Dashboard/PostSummary';
+import FavPostSummary from '../../Components/UI/Dashboard/FavPostSummary';
 
-import { fetchUser } from '../../redux/actions/user'
+import { fetchUser } from '../../redux/actions/user';
+import { fetchChatroom } from '../../redux/actions/chatroom';
 
 // will render signin component if user is not logged in. Still make the route to '/signin' for signin page though
 
 class ConnectedProfileScreen extends Component {
-    static contextType = ThemeContext;
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: [],
-            showLocations: true,
-            showChatrooms: false,
-            showPosts: false,
-            showFav:false
+  static contextType = ThemeContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [],
+      showLocations: true,
+      showChatrooms: false,
+      showPosts: false,
+      showFav: false,
+    };
+  }
 
+  componentWillUnmount() {
+    this.setState({
+      user: [],
+      showLocations: true,
+      showChatrooms: false,
+      showPosts: false,
+      showFav: false,
+    });
+  }
 
-        }
+  async componentDidMount() {
+    let { dispatch } = this.props;
+    let user_id = parseInt(this.props.auth.user.id);
+    console.log(this.props);
+    console.log(user_id);
+
+    await dispatch(fetchUser(user_id));
+
+    if (this.props.user.user) {
+      console.log(this.props.user.user);
+
+      this.setState({
+        user: this.props.user.user,
+        locations: this.props.user.user.locations,
+        chatrooms: this.props.user.user.chatrooms,
+        posts: this.props.user.user.userBlogs,
+        favPosts: this.props.user.user.favBlogs,
+      });
+      console.log(this.state);
     }
+  }
 
-    componentWillUnmount() {
-        this.setState({
-            user: [],
-            showLocations: true,
-            showChatrooms: false,
-            showPosts: false,
-            showFav:false
+  filterLoc = (e) => {
+    const { showLocations } = this.state;
+    this.setState({
+      showLocations: !showLocations,
+      showChatrooms: false,
+      showPosts: false,
+      showFav: false,
+    });
+  };
+  filterCha = (e) => {
+    const { showChatrooms } = this.state;
+    this.setState({
+      showChatrooms: !showChatrooms,
+      showLocations: false,
+      showPosts: false,
+      showFav: false,
+    });
+  };
+  filterPos = (e) => {
+    const { showPosts } = this.state;
+    this.setState({
+      showPosts: !showPosts,
+      showChatrooms: false,
+      showLocations: false,
+      showFav: false,
+    });
+  };
 
-        })
-    }
+  filterFav = (e) => {
+    const { showFav } = this.state;
+    this.setState({
+      showFav: !showFav,
+      showPosts: false,
+      showChatrooms: false,
+      showLocations: false,
+    });
+  };
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
 
-    async componentDidMount() {
-        let { dispatch } = this.props
-        let user_id = parseInt(this.props.auth.user.id)
-        console.log(this.props)
-        console.log(user_id)
+    return (
+      <div
+        id='profile_container'
+        style={{ background: theme.low, color: theme.high }}
+      >
+        <ProfileDetails
+          filterLoc={this.filterLoc}
+          filterCha={this.filterCha}
+          filterPos={this.filterPos}
+          filterFav={this.filterFav}
+          user={this.state.user}
+          locations={this.state.locations}
+          chatrooms={this.state.chatrooms}
+          posts={this.state.posts}
+          favPosts={this.state.favPosts}
+        />
 
-        await dispatch(fetchUser(user_id))
-
-        if (this.props.user.user) {
-            console.log(this.props.user.user)
-
-            this.setState({
-                user: this.props.user.user,
-                locations: this.props.user.user.locations,
-                chatrooms: this.props.user.user.chatrooms,
-                posts: this.props.user.user.userBlogs,
-                favPosts: this.props.user.user.favBlogs,
+        {this.state.locations && this.state.showLocations
+          ? this.state.locations.map((item, i) => {
+              return (
+                <TripSummary
+                  history={this.props.history}
+                  location={item}
+                  key={i}
+                />
+              );
             })
-            console.log(this.state)
-        }
-    }
+          : null}
+        {this.state.chatrooms && this.state.showChatrooms
+          ? this.state.chatrooms.map((item, i) => {
+              return (
+                <GroupSummary
+                  history={this.props.history}
+                  dispatch={this.props.dispatch}
+                  chatroom={item}
+                  key={i}
+                />
+              );
+            })
+          : null}
 
-    filterLoc = (e) => {
+        {this.state.posts && this.state.showPosts
+          ? this.state.posts.map((item, i) => {
+              return (
+                <PostSummary
+                  history={this.props.history}
+                  posts={item}
+                  key={i}
+                />
+              );
+            })
+          : null}
 
-        const { showLocations } = this.state;
-        this.setState({ 
-            showLocations: !showLocations,
-            showChatrooms: false,
-            showPosts: false,
-            showFav:false
-        })
-
-    }
-    filterCha = (e) => {
-
-        const { showChatrooms } = this.state;
-        this.setState({ 
-            showChatrooms: !showChatrooms,
-            showLocations: false,
-            showPosts:false,
-            showFav:false
-        })
-
-    }
-    filterPos = (e) => {
-
-        const { showPosts } = this.state;
-        this.setState({ 
-            showPosts: !showPosts,
-            showChatrooms: false,
-            showLocations:false,
-            showFav:false
-        })
-
-    }
-
-    filterFav = (e) => {
-
-        const { showFav } = this.state;
-        this.setState({ 
-            showFav: !showFav,
-            showPosts: false,
-            showChatrooms: false,
-            showLocations:false,
-        })
-
-    }
-    render() {
-
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-
-        return (
-            <div id="profile_container" style={{ background: theme.low, color: theme.high }}>
-
-
-                <ProfileDetails
-                    filterLoc={this.filterLoc}
-                    filterCha={this.filterCha}
-                    filterPos={this.filterPos}
-                    filterFav={this.filterFav}
-                    user={this.state.user}
-                    locations={this.state.locations}
-                    chatrooms={this.state.chatrooms}
-                    posts={this.state.posts}
-                    favPosts={this.state.favPosts} />
-
-                {this.state.locations && this.state.showLocations ?
-                    this.state.locations.map((item, i) => {
-                        return <TripSummary history={this.props.history} location={item} key={i} />
-                    }) : null
-                }
-                {this.state.chatrooms && this.state.showChatrooms ?
-                    this.state.chatrooms.map((item, i) => {
-                        return <GroupSummary history={this.props.history} chatroom={item} key={i} />
-                    }) : null
-                }
-
-                {this.state.posts && this.state.showPosts ?
-                    this.state.posts.map((item, i) => {
-                        return <PostSummary history={this.props.history} posts={item} key={i} />
-                    }) : null
-                }
-
-                {this.state.favPosts && this.state.showFav ?
-                    this.state.favPosts.map((item, i) => {
-                        return <FavPostSummary history={this.props.history} favPosts={item} key={i} />
-                    }) : null
-                }
-
-
-
-
-            </div>
-
-        )
-    }
+        {this.state.favPosts && this.state.showFav
+          ? this.state.favPosts.map((item, i) => {
+              return (
+                <FavPostSummary
+                  history={this.props.history}
+                  favPosts={item}
+                  key={i}
+                />
+              );
+            })
+          : null}
+      </div>
+    );
+  }
 }
-
-        
 
 const mapStateToProps = (state) => {
   return {
