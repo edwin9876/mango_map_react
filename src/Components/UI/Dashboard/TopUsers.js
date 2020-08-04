821dd4e21345e8890cdbb97298e55e48c9f64a18
YIN <anotherone@YINde-Air.lan>
2020-08-04 20:43:27 +0800

    ee

--- src/Components/UI/Dashboard/TopUsers.js
+++ src/Components/UI/Dashboard/TopUsers.js
@@ -6,13 +6,17 @@ const TopUsers = ({ user }) => {
     console.log(user)
     return (
         <div className="margin1x">
-        <Container className="bgdark noBorder">
-            <Row>
-                <Col xs="3" m="2" l="1"><img src={user.profile_picture_url} alt="profilepic" className="icons20 rBorder" />
-                    <p className="white100">{user.user_name}</p></Col>
-            </Row>
-        </Container>
-    </div>
+            <Container className="bgdark noBorder">
+                <Row>
+                    <Col xs="3" m="2" l="1">
+                        {user.profile_picture_url && user.profile_picture_url.length >= 200 ?
+                            <img src={`data:image/png;base64, ${user.profile_picture_url}`} alt="profilepic" className="icons20 rBorder" /> :
+                            <img src={user.profile_picture_url} className="icons20 rBorder" id="profile_pic" />
+                        }
+                        <p className="white100">{user.user_name}</p></Col>
+                </Row>
+            </Container>
+        </div>
 
     )
 }
