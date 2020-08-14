8660431a92debe1a7cfadd27bdaa6262bb752697
ch-oi <gmu1233@gmail.com>
2020-08-15 01:52:12 +0800

    Fixed the history.object being undefined in group map

--- src/Components/Chatroom/CreateTrip.js
+++ src/Components/Chatroom/CreateTrip.js
@@ -1,72 +1,96 @@
 import React, { Component } from 'react';
-import { ThemeContext } from '../../Contexts/Theme'
+import { ThemeContext } from '../../Contexts/Theme';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-
-
 
 class CreateTrip extends Component {
   static contextType = ThemeContext;
   constructor(props) {
-    super(props)
-    this.state = {
-
-    };
+    super(props);
+    this.state = {};
   }
 
-  handleChange = (e) => {
+  handleChange = () => {};
 
-  }
-
-  handleSubmit = async (e) => {
-
-
-  }
-
+  handleSubmit = async () => {};
 
   render() {
+    console.log(this.props);
+
     const { isLightTheme, light, dark } = this.context;
     const theme = isLightTheme ? light : dark;
 
     return (
-      <div className="margin5" id="Post_container" style={{ background: theme.low, color: theme.high }}>
-        <div className='d-flex justify-content-between paddingt1 '>
-          <i className='material-icons gray50'>
-            arrow_back
-      </i>
+      <div
+        className="margin5"
+        id="Post_container"
+        style={{ background: theme.low, color: theme.high }}
+      >
+        <div
+          className="d-flex justify-content-between paddingt1"
+          onClick={() => this.props.history.push('/chat')}
+        >
+          <i className="material-icons gray50">arrow_back</i>
         </div>
         <div className="margin5x paddingt1">
-          <Label for="date" className="bold">Location</Label>
+          <Label for="date" className="bold">
+            Location
+          </Label>
           <p classNam="gray70">Lizard's back, New Territories</p>
         </div>
-        <Form id="createPost" onSubmit={this.handleSubmit} className="uploader margin5" encType="multipart/form-data">
-
+        <Form
+          id="createPost"
+          onSubmit={this.handleSubmit}
+          className="uploader margin5"
+          encType="multipart/form-data"
+        >
           <FormGroup>
-
-            <Label for="date" className="bold">Date and time</Label>
-            <Input style={{ background: theme.low, borderColor: theme.highlight, color: theme.high }} onChange={this.handleChange} type="text" name="date" placeholder="Date and time" />
+            <Label for="date" className="bold">
+              Date and time
+            </Label>
+            <Input
+              style={{
+                background: theme.low,
+                borderColor: theme.highlight,
+                color: theme.high,
+              }}
+              onChange={this.handleChange}
+              type="text"
+              name="date"
+              placeholder="Date and time"
+            />
           </FormGroup>
 
           <FormGroup>
-            <Label for="description" className="bold">Description</Label>
-            <Input style={{ background: theme.low, borderColor: theme.highlight, color: theme.high }} onChange={this.handleChange} type="text" name="description" placeholder="Description" />
+            <Label for="description" className="bold">
+              Description
+            </Label>
+            <Input
+              style={{
+                background: theme.low,
+                borderColor: theme.highlight,
+                color: theme.high,
+              }}
+              onChange={this.handleChange}
+              type="text"
+              name="description"
+              placeholder="Description"
+            />
           </FormGroup>
 
           <div className="d-flex justify-content-center">
-            <Button style={{ backgroundColor: theme.highlight }} className="btn margin5 noBorder"
-              type="submit" name="action" >Create Trip
-        </Button>
+            <Button
+              style={{ backgroundColor: theme.highlight }}
+              className="btn margin5 noBorder"
+              type="submit"
+              name="action"
+            >
+              Create Trip
+            </Button>
           </div>
-
         </Form>
-
       </div>
-
     );
   }
 }
-
-
-
-
 
 export default CreateTrip;
--- src/Containers/Map/IndiMap.js
+++ src/Containers/Map/IndiMap.js
@@ -1,84 +1,81 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import ReactDOM from "react-dom";
-import { Map, InfoWindow, GoogleApiWrapper, Marker } from "google-maps-react";
-import { connect } from "react-redux";
-import axios from "axios";
-import predefinedLocations from "./PredefinedLocations/LocationStorage";
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { Map, InfoWindow, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import predefinedLocations from './PredefinedLocations/LocationStorage';
 import {
   Button,
   Dropdown,
   DropdownMenu,
   DropdownToggle,
   DropdownItem,
-} from "reactstrap";
+} from 'reactstrap';
 
-import simple from "./mapStyle_simple";
+import simple from './mapStyle_simple';
 import {
   fetchAllDistricts,
   changeZoomLevel,
   fetchAllLocations,
   saveLatLng,
   fetchLocation,
-} from "../../redux/actions/map";
-import { fetchChatroomList, fetchChatroom } from "../../redux/actions/chatroom";
-import { useReducedMotion } from "framer-motion";
+} from '../../redux/actions/map';
+import { fetchChatroomList, fetchChatroom } from '../../redux/actions/chatroom';
+import { useReducedMotion } from 'framer-motion';
 
 const mapStyles = {
-  width: "100%",
-  height: "100%",
+  width: '100%',
+  height: '100%',
 };
 
-const user = JSON.parse(localStorage.getItem("user"));
+const user = JSON.parse(localStorage.getItem('user'));
 
-// Testing
+const MapContainer = (props) => {
+  const [showInfoWindow, setShowInfoWindow] = useState(false);
+  const [activeMarker, setActiveMarker] = useState({});
+  const [selectedPlace, setSelectedPlace] = useState({});
+  const [currentLocation, setCurrentLocation] = useState({
+    lat: null,
+    lng: null,
+  });
+  const [locationId, setLocationId] = useState(null);
+  const [selectedPlaceImages, setSelectedPlaceImages] = useState([]);
+  const [selfDefinedMarkers, setSelfDefinedMarkers] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [chatrooms, setChatrooms] = useState([]);
 
-export class MapContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.mapRefs = React.createRef();
+  const history = useHistory();
+  if (!props.history) {
+    const newProps = Object.assign({ ...props }, { history });
+    props = newProps;
   }
 
-  state = {
-    showingInfoWindow: false, //Hides or the shows the infoWindow
-    activeMarker: {}, //Shows the active marker upon click
-    selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
-    currentLocation: {
-      lat: null,
-      lng: null,
-    },
-    locations_id: null,
-    selectedPlaceImages: [],
-    // This must be stored in database
-    selfDefinedMarkers: [],
-    dropdownOpen: false,
-    chatrooms: [],
-  };
-
-  componentDidMount() {
-    this.props.fetchAllDistricts();
-    this.props.fetchAllLocations();
+  useEffect(() => {
+    props.fetchAllDistricts();
+    props.fetchAllLocations();
     if (user) {
-      console.log(user.id);
       axios
         .get(`${process.env.REACT_APP_DEV_URL}chatroom/all/${user.id}`)
         .then((data) => {
-          console.log(data);
-          this.setState({
-            ...this.state,
+          setChatrooms({
             chatrooms: [...data.data],
           });
         });
     }
-  }
+  }, []);
 
-  _mapLoaded(mapProps, map) {
+  useEffect(() => {
+    console.log(selectedPlace);
+  }, [selectedPlace]);
+
+  const _mapLoaded = (mapProps, map) => {
     map.setOptions({
       styles: simple,
     });
-  }
+  };
 
-  changeTheme(mapProps, map) {
+  const changeTheme = (mapProps, map) => {
     map.setOptions.styles === simple
       ? map.setOptions({
           styles: mapStyles,
@@ -86,215 +83,174 @@ export class MapContainer extends Component {
       : map.setOptions({
           styles: simple,
         });
-  }
-
-  onMarkerClick = (props, marker, e) => {
-    this.setState(
-      {
-        ...this.state,
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true,
-      },
-      () => {
-        if (!this.state.selectedPlace.id) {
-          console.log("Return");
-          return;
-        }
-        axios
-          .get(
-            `${process.env.REACT_APP_DEV_URL}image/public/${this.state.selectedPlace.id}`
-          )
-          .then((data) => {
-            this.setState({
-              ...this.state,
-              selectedPlaceImages: data.data,
-            });
-          });
-      }
-    );
   };
 
-  onClose = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState(
-        {
-          ...this.state,
-          showingInfoWindow: false,
-          activeMarker: null,
-          selectedPlaceImages: null,
-          selectedPlace: {},
-        },
-        () => {
-          console.log(props);
-        }
-      );
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPlace(props);
+    setActiveMarker(marker);
+    setShowInfoWindow(true);
+  };
+
+  const onClose = (props) => {
+    if (showInfoWindow) {
+      setShowInfoWindow(false);
+      setActiveMarker(null);
+      setSelectedPlaceImages(null);
+      setSelectedPlace({});
     }
   };
 
-  createMarker = (lat, lng) => {
-    this.setState(
-      {
-        ...this.state,
-        selfDefinedMarkers: [...this.state.selfDefinedMarkers, { lat, lng }],
-      },
-      () => {
-        console.log(this.state.selfDefinedMarkers);
-      }
-    );
+  const createMarker = (lat, lng) => {
+    setSelfDefinedMarkers([...selfDefinedMarkers, { lat, lng }]);
   };
 
-  createTrip = (e) => {
-    this.props.history.push("/createtrip");
+  const createTrip = (e) => {
+    console.log('Clicked');
+    console.log(selectedPlace);
+    props.history.push(`/createtrip?${selectedPlace.name}`);
   };
 
-  markTripLocation = () => {};
+  const markTripLocation = () => {};
 
-  createLocation = (e) => {
-    this.props.saveLatLng(this.state.selectedPlace.position);
-    console.log(this.props);
-    this.props.history.push("/createlocation");
+  const createLocation = (e) => {
+    props.saveLatLng(this.state.selectedPlace.position);
+    props.history.push('/createlocation');
   };
 
-  onInfoWindowOpen(props, e) {
+  const onInfoWindowOpen = (props, e) => {
     const button = (
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
+          display: 'flex',
+          flexDirection: 'column',
           height: 100,
-          justifyContent: "space-around",
+          justifyContent: 'space-around',
         }}
       >
-        {!this.state.selectedPlace.id &&
-        !this.state.selectedPlace.district &&
-        user ? (
-          <Button history={this.props.history} onClick={this.createTrip}>
+        {!selectedPlace.id && !selectedPlace.district && user ? (
+          <Button history={props.history} onClick={() => createTrip()}>
             New Trip
           </Button>
-        ) : this.state.selectedPlace.location && user ? (
+        ) : selectedPlace.location && user ? (
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
+              display: 'flex',
+              flexDirection: 'column',
               height: 100,
-              justifyContent: "space-around",
+              justifyContent: 'space-around',
             }}
           ></div>
         ) : null}
       </div>
     );
-    ReactDOM.render(
-      React.Children.only(button),
-      document.getElementById("iwc")
-    );
-    console.log(this.state);
-  }
-
-  toggle = (e) => {
-    let { name, id } = this.state.selectedPlace;
-    this.props.fetchChatroom(e.target.value, { name, id });
-    console.log(this.props);
-    this.props.history.push(`/chat`);
+    // ReactDOM.render(
+    //   React.Children.only(button),
+    //   document.getElementById('iwc')
+    // );
   };
 
-  render() {
-    let locations;
-    let selfDefinedMarkers;
+  const toggle = (e) => {
+    let { name, id } = this.state.selectedPlace;
+    props.fetchChatroom(e.target.value, { name, id });
+    props.history.push(`/chat`);
+  };
 
-    locations = this.props.districts.map((location) => {
+  let locations;
+  let selfDefinedMarkersDisplay;
+
+  locations = props.locations.map((location) => {
+    return (
+      <Marker
+        icon={{
+          url: './assets/icons/adventure2.png',
+          anchor: new window.google.maps.Point(25, 25),
+          scaledSize: new window.google.maps.Size(45, 45),
+        }}
+        key={location.id}
+        id={location.id}
+        position={{ lat: location.lat, lng: location.lng }}
+        onClick={onMarkerClick}
+        name={`${location.en} ${location.cn}`}
+        location={true}
+      />
+    );
+  });
+
+  if (selfDefinedMarkers) {
+    selfDefinedMarkersDisplay = selfDefinedMarkers.map((marker) => {
       return (
         <Marker
-          icon={{
-            url: "./assets/icons/adventure2.png",
-            anchor: new window.google.maps.Point(25, 25),
-            scaledSize: new window.google.maps.Size(45, 45),
+          position={{
+            lat: marker.lat,
+            lng: marker.lng,
           }}
-          key={location.id}
-          id={location.id}
-          position={{ lat: location.lat, lng: location.lng }}
-          onClick={this.onMarkerClick}
-          name={`${location.en} ${location.cn}`}
-          location={true}
+          onClick={onMarkerClick}
+          name="Should we visit?"
         />
       );
     });
-
-    if (this.state.selfDefinedMarkers) {
-      selfDefinedMarkers = this.state.selfDefinedMarkers.map((marker) => {
-        return (
-          <Marker
-            position={{
-              lat: marker.lat,
-              lng: marker.lng,
-            }}
-            onClick={this.onMarkerClick}
-            name="Should we visit?"
-          />
-        );
-      });
-    }
-
-    return (
-      <div>
-        <Map
-          ref={this.mapRefs}
-          onZoomChanged={(google, map) => {
-            this.props.changeZoomLevel(map.zoom);
-          }}
-          onLoad={(map) => console.log(map)}
-          onClick={(props, google, clickEvent) => {
-            const lat = clickEvent.latLng.lat();
-            const lng = clickEvent.latLng.lng();
-            console.log(google);
-            this.createMarker(lat, lng);
-          }}
-          centerAroundCurrentLocation
-          google={this.props.google}
-          zoom={13}
-          initialCenter={{
-            // TODO - default location
-            lat: this.state.currentLocation.lat,
-            lng: this.state.currentLocation.lng,
-          }}
-          onReady={(mapProps, map) => this._mapLoaded(mapProps, map)}
-        >
-          <Marker
-            icon={{
-              url: "./assets/icons/user.png",
-              anchor: new window.google.maps.Point(25, 25),
-              scaledSize: new window.google.maps.Size(50, 50),
-            }}
-            position={{
-              lat: this.state.currentLocation.lat,
-              lng: this.state.currentLocation.lng,
-            }}
-            onClick={this.onMarkerClick}
-            name="You are here"
-          />
-          {selfDefinedMarkers}
-          {locations}
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-            onClose={this.onClose}
-            onOpen={(e) => {
-              this.onInfoWindowOpen(this.props, e);
-            }}
-          >
-            <div className="vw50">
-              <h5 className="bold gray70 d-flex justify-content-center">
-                {this.state.selectedPlace.name}
-              </h5>
-              <div className="d-flex justify-content-center"></div>
-              <div id="iwc" />
-            </div>
-          </InfoWindow>
-        </Map>
-      </div>
-    );
   }
-}
+
+  return (
+    <div>
+      <Map
+        onZoomChanged={(google, map) => {
+          props.changeZoomLevel(map.zoom);
+        }}
+        onClick={(props, google, clickEvent) => {
+          const lat = clickEvent.latLng.lat();
+          const lng = clickEvent.latLng.lng();
+          createMarker(lat, lng);
+        }}
+        centerAroundCurrentLocation
+        google={props.google}
+        zoom={13}
+        initialCenter={{
+          // TODO - default location
+          lat: currentLocation.lat,
+          lng: currentLocation.lng,
+        }}
+        onReady={(mapProps, map) => _mapLoaded(mapProps, map)}
+      >
+        <Marker
+          icon={{
+            url: './assets/icons/user.png',
+            anchor: new window.google.maps.Point(25, 25),
+            scaledSize: new window.google.maps.Size(50, 50),
+          }}
+          position={{
+            lat: currentLocation.lat,
+            lng: currentLocation.lng,
+          }}
+          onClick={onMarkerClick}
+          name="You are here"
+        />
+        {selfDefinedMarkersDisplay}
+        {locations}
+        <InfoWindow
+          marker={activeMarker}
+          visible={showInfoWindow}
+          onClose={onClose}
+          onOpen={(e) => {
+            onInfoWindowOpen(props, e);
+          }}
+        >
+          <div className="vw50">
+            <h5 className="bold gray70 d-flex justify-content-center">
+              {selectedPlace.name}
+            </h5>
+            <div className="d-flex justify-content-center">
+              <Button history={props.history} onClick={createTrip}>
+                New Trip
+              </Button>
+            </div>
+            {/* <div id="iwc" /> */}
+          </div>
+        </InfoWindow>
+      </Map>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
